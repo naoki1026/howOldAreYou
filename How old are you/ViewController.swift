0ca1d543e5e4c1adb116cd752c122b36676cbd1f
Naoki Arakawae <naoki.arakawa1026@gmail.com>
2019-02-18 12:20:02 +0900

    firstCommit

--- How old are you/ViewController.swift
+++ How old are you/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var howOld: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func clickMe(_ sender: Any) {
+        
+        if let age = howOld.text {
+            
+            label.text =  "You are \(age) years old"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
